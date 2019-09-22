6a705e2e552758a2f8a6a7bce13eb9c8a03fe8bd
ljxzsx4 <ljxzsx4@gmail.com>
2019-09-22 13:35:42 +0800

    learn 2.3.2 and 2.3.3

--- keras_ex.py
+++ keras_ex.py
@@ -17,7 +17,6 @@ keras.layers.Dense(512, activation='relu')
 @ps.snoop()
 def naive_relu(x):
     assert len(x.shape) == 2
-
     # deep copy
     x = x.copy()
     # return range of index
@@ -52,3 +51,47 @@ t = np.maximum(1., z)
 print(z)
 print(t)
 # python3 keras_ex.py
+
+
+# 2.3.2
+y = np.array([5, 78, 2, 24, 0])
+
+@ps.snoop()
+def naive_add_matrix_and_vector(x, y):
+    assert len(x.shape) == 2
+    assert len(y.shape) == 1
+    assert x.shape[1] == y.shape[0]
+
+    x = x.copy()
+    # range(3), [0, 1, 2]
+    for i in range(x.shape[0]):
+        # range(5), [0, 1, 2, 3, 4]
+        for j in range(x.shape[1]):
+            x[i, j] += y[j]
+    return x
+
+naive_add_matrix_and_vector(x, y)
+
+x = np.random.random((64, 3, 32, 10))
+y = np.random.random((32, 10))
+
+z = np.maximum(x, y)
+
+# 2.3.3
+# z = np.dot(x ,y)
+#z = x.y
+
+@ps.snoop()
+def naive_vector_dot(x, y):
+    assert len(x.shape) == 1
+    assert len(y.shape) == 1
+    assert x.shape[0] == y.shape[0]
+
+    z = 0.
+    for i in range(x.shape[0]):
+        z += x[i] * y[i]
+    return z
+
+x =  np.array([5, 78, 2, 24, 0])
+y =  np.array([5, 78, 2, 24, 0])
+naive_vector_dot(x, y)
