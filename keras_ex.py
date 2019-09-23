c7452de6b4b10efb4c743f5369a905d9503cca54
ljxzsx4 <ljxzsx4@gmail.com>
2019-09-23 10:19:26 +0800

    compete 2.3.3

--- keras_ex.py
+++ keras_ex.py
@@ -126,3 +126,24 @@ def naive_matrix_vector_dot(x, y):
     return z
 
 naive_matrix_vector_dot(x, y)
+
+x =  np.array([[8, 78],
+               [5, 78]])
+y =  np.array([[8, 78],
+               [5, 78]])
+
+@ps.snoop()
+def naive_matrix_dot(x, y):
+    assert len(x.shape) == 2
+    assert len(y.shape) == 2
+    assert x.shape[1] == y.shape[0]
+
+    z = np.zeros((x.shape[0], y.shape[1]))
+    for i in range(x.shape[0]):
+        for j in range(y.shape[1]):
+            row_x = x[i, :]
+            column_y = y[:, j]
+            z[i, j] = naive_vector_dot(row_x, column_y)
+    return z
+    
+naive_matrix_dot(x, y)
